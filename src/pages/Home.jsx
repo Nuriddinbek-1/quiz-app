d2d847e0aa34e07ce5a79b297999d6cc7882f922
Nuriddinbek Obidjonov <nuriddinbek.obidjonov@gmail.com>
2025-09-21 18:58:13 +0500

    home is being created

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,16 @@
 function Home() {
-  return <div>Home</div>;
+  return (
+    <section className="container home-container">
+      <div className="home-content">
+        <h1 className="home-title">
+          <span>Welcome to the</span>
+          <span>Frontend Quiz</span>
+        </h1>
+        <p>Pick a subject to get started</p>
+      </div>
+      <div className="home-nav-list"></div>
+    </section>
+  );
 }
 
 export default Home;
